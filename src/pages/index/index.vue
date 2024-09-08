c3b81469162498cad831bcf75beb16bdd6147857
WZ <2072401390@qq.com>
2024-09-08 21:41:38 +0800

    init

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,49 +1,22 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
-		</view>
-	</view>
+<view class="layout">
+	<view class="box1">box1</view>
+	<view class="box2">box2</view>
+</view>
 </template>
 
-<script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
+<script setup>
 
-		},
-		methods: {
-
-		}
-	}
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
+<style lang="scss">
+.layout{
+	border:1px solid red;
+	.box1{
+		border:1px slod green;
 	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin: 200rpx auto 50rpx auto;
+	.box2{
+		border: 1px solid blue
 	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+}
 </style>
